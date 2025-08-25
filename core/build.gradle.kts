22102e04e022a6fb0acf994732208887e4f0fd82
girlforsweet <163544378+girlforsweet@users.noreply.github.com>
2025-08-25 21:44:06 +0800

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,24 +1,4 @@
-/*
- * This file is part of LSPosed.
- *
- * LSPosed is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LSPosed is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
- *
- * Copyright (C) 2021 - 2022 LSPosed Contributors
- */
-
-val verName: String by rootProject.extra
-val verCode: Int by rootProject.extra
+import org.gradle.api.provider.Provider
 
 plugins {
     alias(libs.plugins.agp.lib)
@@ -35,24 +15,33 @@ android {
     defaultConfig {
         consumerProguardFiles("proguard-rules.pro")
 
-        buildConfigField("String", "FRAMEWORK_NAME", """"${rootProject.name}"""")
-        buildConfigField("String", "VERSION_NAME", """"$verName"""")
-        buildConfigField("long", "VERSION_CODE", """$verCode""")
+        val verName: String by rootProject.extra
+        val verCode: Int by rootProject.extra
+        buildConfigField("String", "FRAMEWORK_NAME", "\"${rootProject.name}\"")
+        buildConfigField("String", "VERSION_NAME", "\"$verName\"")
+        buildConfigField("long", "VERSION_CODE", "$verCode")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = true
-            proguardFiles("proguard-rules.pro")
         }
     }
 }
 
-copy {
-    from("src/main/jni/template/") {
-        expand("VERSION_CODE" to "$verCode", "VERSION_NAME" to verName)
+val verName: String by rootProject.extra
+val verCode: Int by rootProject.extra
+val rootDir: Provider<String> = layout.projectDirectory.asFile.map { it.absolutePath }
+tasks.register("copyTemplate") {
+    dependsOn("preBuild")
+    doLast {
+        copy {
+            from("src/main/jni/template/") {
+                expand("VERSION_CODE" to "$verCode", "VERSION_NAME" to verName)
+            }
+            into("src/main/jni/src/")
+        }
     }
-    into("src/main/jni/src/")
 }
 
 dependencies {
